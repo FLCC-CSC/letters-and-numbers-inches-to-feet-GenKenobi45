58b8b5dec1d4a77240b624bf7fd1cfd96e72c585
GenKenobi45 <ykhan2@fingerlakes.edu>
2025-02-16 22:54:06 -0500

    Update inches_to_feet.py

--- inches_to_feet.py
+++ inches_to_feet.py
@@ -1,8 +1,9 @@
 # FILE NAME - inches_to_feet.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Yusuf Khan 
+# DATE: 02/13/25
+# BRIEF DESCRIPTION: Program that oputputs the area of a triangle based on the height and 
+# base lengths entered by the user.
 
 
 
@@ -14,21 +15,15 @@
 
 
 
-
-# BEWARE! You'll need to cast not only the input from the user
-# but also maybe the number of feet
-
 ########## ENTER YER CODE BELOW THIS LINE ##########
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+def find_area():   
+    height = float(input('Enter the height: '))
+    base = float(input('Enter the base: '))
+    area = 0.5*base*height
+    print('The area of the triangle is', area)
+find_area()
+
 ########### END YER CODE ABOVE THIS LINE ###########
 
 
@@ -41,20 +36,21 @@
 
 '''
 
-Enter the number of inches: 14
+Enter the height: 1
+Enter the base: 1
 
-14 inches is 1 feet, and 2 inches
+The area of the triangle is 0.5
+
+'''
+
 
 
 '''
 
+Enter the height: 8
+Enter the base: 4
 
-
-'''
-
-Enter the number of inches: 100
-
-100 inches is 8 feet, and 4 inches
+The area of the triangle is 16.0
 
 '''
 
@@ -66,9 +62,16 @@ Enter the number of inches: 100
 
 '''
 
-1. What does it mean to "cast" input from the user?
+1. What is the flow of the program? Which line of code kicks off the process?
 
+I feel that the second line of this code kicks off the process becasue it is the first line 
+asking for user input, and without that input the program would not be able to output a 
+correct answer.
 
+2. What was the hardest part of this lab?
+
+The hardest of the part of this lab was to remeber to use the float funtion to make 
+sure the integer can be multiplied by the text.
 
 
 
